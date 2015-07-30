04c881a359207cf80dd953080e8e05e628151bf2
Luke Jones <luke@freerunningtechnologies.com>
2015-07-30 09:54:32 -0700

    Add jObject public api for module
    this commit now lets users use JObject.parse and .parse_each to parse
    json. .parse is used for a single json object, .parse_each is if your
    json starts off with a list of objects.

--- lib/jObject.rb
+++ lib/jObject.rb
@@ -1,5 +1,17 @@
 require "jObject/version"
+require 'jObject/jObject'
+require 'json'
 
 module JObject
-  # Your code goes here...
+  def self.parse(json)
+    data_hash = JSON.parse(json)
+    JObject.new(data_hash)
+  end
+
+  def self.parse_each(json)
+    data_array = JSON.parse(json)
+    data_array.map do |hash|
+      JObject.new(hash)
+    end
+  end
 end
--- spec/jObject_spec.rb
+++ spec/jObject_spec.rb
@@ -5,7 +5,27 @@ describe JObject do
     expect(JObject::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".parse" do
+    subject { described_class.parse(json) }
+
+    let(:json) { '{ "name": "bob" }' }
+
+    it "should return a parsed jObject" do
+      expect(subject).to be_a JObject::JObject
+      expect(subject).to have_attributes(name: "bob")
+    end
+  end
+
+  describe ".parse_each" do
+    subject { described_class.parse_each(json) }
+
+    let(:json) { '[{ "name": "bob" }, { "name": "george" }]' }
+
+    it "should return a parsed array of jObjects" do
+      expect(subject).to be_an Array
+      expect(subject).to all be_a JObject::JObject
+      expect(subject[0].name).to eq "bob"
+      expect(subject[1].name).to eq "george"
+    end
   end
 end
